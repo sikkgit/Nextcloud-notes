14edf2df2e08e0692ceadd15e7f3090ec5f15e4c
Luka Trovic <luka@nextcloud.com>
2024-09-10 19:11:53 +0200

    feat: add SPDX headers
    Signed-off-by: Luka Trovic <luka@nextcloud.com>

--- templates/main.php
+++ templates/main.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * SPDX-FileCopyrightText: 2013 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 
 script('notes', 'notes-main');
 style('notes', 'notes');
